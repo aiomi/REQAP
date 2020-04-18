d5168f7a3a60bc9dfd47e9279d8e0ac3156a00e3
David Onuh <youngstizy@gmail.com>
2020-04-18 23:00:57 +0100

    remove students model

--- users/models.py
+++ users/models.py
@@ -1,35 +1,25 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, Group
 # Create your models here.
 
 class User(AbstractUser):
-    school = (
-        ('sict','SICT'),
-        ('set','SET')
-    )
-    faculty = models.CharField(max_length=12, choices=school)
+    
+    faculty = models.CharField(max_length=12)
     department = models.CharField(max_length=65)
 
     is_teacher = models.BooleanField(default=False)
     is_student = models.BooleanField(default=False)
+    #has_graduated = models.BooleanField(default=False)
 
 
-
-class Student(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    gradution_date = models.DateField()
-    has_graduated = models.BooleanField(default=False)
+    def __str__(self):
+        return f'User: {self.first_name}'
 
 class Staff(models.Model):
-    """
-    For now this takes care of only Academic staffs
-    """
-    staff_offices = (
-        ('normal', 'Normal'),
-        ('LA', 'Level Adviser'),
-        ('EO', 'Exam officer'),
-        ('HOD', 'Head of Department'),
-        ('DOS', 'Dean of School')
-    )
+
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    office = models.CharField(max_length=25, choices=staff_offices)
+    #office = models.CharField(max_length=25, choices=staff_offices)
+    office = models.ForeignKey(Group, on_delete=models.DO_NOTHING, blank=True, null=True)
+
+    def __str__(self):
+        return f'staff {self.user.first_name}'
