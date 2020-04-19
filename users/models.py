6c9f18b5d84613510e051805bf717f3431d8d2cb
David Onuh <youngstizy@gmail.com>
2020-04-19 15:32:05 +0100

    edit max_length for dept to 20

--- users/models.py
+++ users/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, Group
+from PIL import Image
 # Create your models here.
 
 class User(AbstractUser):
     
     faculty = models.CharField(max_length=12, blank=True, null=True)
-    department = models.CharField(max_length=65, blank=True, null=True)
+    department = models.CharField(max_length=20, blank=True, null=True)
 
     is_teacher = models.BooleanField(default=False)
     is_student = models.BooleanField(default=False)
@@ -23,3 +24,23 @@ class Staff(models.Model):
 
     def __str__(self):
         return f'staff {self.user.first_name}'
+
+    
+
+# class StaffVerification(models.Model):
+#     staff = models.OneToOneField(Staff, on_delete=models.CASCADE)
+#     selfie = models.ImageField(upload_to='staffs', blank=True, null=True)
+#     id_card = models.ImageField(upload_to='staffs', blank=True, null=True)
+#     is_verified = models.BooleanField(default=False)
+
+#     def save(self):
+#         super().save()
+#         img = Image.open(self.selfie.path)
+#         if img.height > 500 or img.width > 500:
+#             output_size = (500,500)
+#             img.save(self.img.path)
+
+#         img_ = Image.open(self.id_card.path)
+#         if img_.height > 500 or img_.width > 500:
+#             output_size = (500,500)
+#             img_.save(self.img_.path)
