43e20021dc8cc913c004153ab3bc43453f691ebc
David Onuh <youngstizy@gmail.com>
2020-04-20 00:03:31 +0100

    add date_created to staff verification models
    customize models in admin

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, Group
+from django.utils import timezone
 from PIL import Image
 # Create your models here.
 
@@ -32,6 +33,7 @@ class StaffVerification(models.Model):
     selfie = models.ImageField(upload_to='staffs', blank=True, null=True)
     id_card = models.ImageField(upload_to='staffs', blank=True, null=True)
     is_verified = models.BooleanField(default=False)
+    date_created = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return f'{self.staff.user.username} verification files'
