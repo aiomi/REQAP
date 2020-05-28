45364ffbff984983095fab3bf1c94c10b44a6a09
David Onuh <youngstizy@gmail.com>
2020-05-28 10:21:20 +0100

    add apply for leave view

--- leave/views.py
+++ leave/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 
+from .forms import LeaveForm
+from .models import Leave
 # Create your views here.
+
+
+def apply_for_leave(request):
+    form = LeaveForm()
+    if request.method=="POST":
+        if form.is_valid():
+            form = LeaveForm(request.POST or None)
+            form.save()
+    context = {'form':form}
+    return render(request, 'leave.html', context=context)
