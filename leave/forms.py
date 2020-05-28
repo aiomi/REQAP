ac4961a21bd6a35dfdf6d3df1f06454aba6bb714
David Onuh <youngstizy@gmail.com>
2020-05-28 10:20:49 +0100

    leave form completed

--- leave/forms.py
+++ leave/forms.py
@@ -6,3 +6,11 @@ class LeaveForm(forms.ModelForm):
     class Meta:
         model = Leave
         exclude = ['status', ]
+
+    def clean(self):
+        cleaned_data = super().clean()
+        start_date = cleaned_data.get('from_date')
+        end_date = cleaned_data.get('to_date')
+
+        if end_date < start_date:
+            raise forms.ValidationError('End date should be greater than start date')
