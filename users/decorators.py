d4e38ac885f6210ab64f27f28a2b1d293ecd3160
David Onuh <youngstizy@gmail.com>
2020-05-30 14:38:01 +0100

    change teacher decorator to show messages if condition fails

--- users/decorators.py
+++ users/decorators.py
@@ -1,4 +1,6 @@
 from django.core.exceptions import PermissionDenied
+from django.shortcuts import reverse, redirect
+from django.contrib import messages
 from django.contrib.auth import REDIRECT_FIELD_NAME
 from django.contrib.auth.decorators import user_passes_test
 from django.contrib.auth.models import Group
@@ -7,20 +9,16 @@ from django.shortcuts import get_object_or_404
 from users.models import Staff
 
 
-def teacher_required(function=None, redirect_field_name=REDIRECT_FIELD_NAME, login_url='login'):
+def teacher_required(function):
     '''
     Decorator for views that checks that the logged in user is a teacher,
     redirects to the log-in page if necessary.
     '''
-    actual_decorator = user_passes_test(
-        #u.is_active no need to check because staffs are set to inactive by default
-        lambda u: u.is_teacher, 
-        login_url=login_url,
-        redirect_field_name=redirect_field_name
-        )
-    if function:
-        return actual_decorator(function)
-    return actual_decorator
+    def wrap(request, *args, **kwargs):
+        if not request.user.is_teacher:
+            messages.info(request, "Only staffs required")
+            return redirect(reverse('login'))
+    return wrap
 
 def user_is_student_or_acadoffice_staff(function):
     """
