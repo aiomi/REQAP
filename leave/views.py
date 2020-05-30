cb201dea1bfc585b1f4b0f8ed902a9e3c4a608e8
David Onuh <youngstizy@gmail.com>
2020-05-30 14:38:37 +0100

    add login_required and teacher required decorator to apply for leave view

--- leave/views.py
+++ leave/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render, redirect, reverse
-
+from django.contrib.auth.decorators import login_required
+from users.decorators import teacher_required
 from .forms import LeaveForm
 from .models import Leave
 # Create your views here.
 
 
+
+@login_required
+@teacher_required
 def apply_for_leave(request):
     form = LeaveForm()
     if request.method=="POST":
