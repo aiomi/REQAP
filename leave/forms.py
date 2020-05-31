70bbe85d9e17d3023da8dad5476b48bc9b861672
David Onuh <youngstizy@gmail.com>
2020-05-31 01:00:54 +0100

    add admin multimodelchoice select widget apply for leave form

--- leave/forms.py
+++ leave/forms.py
@@ -1,12 +1,27 @@
 from django import forms
+from django.contrib.admin.widgets import FilteredSelectMultiple
+from datetime import date
+from users.models import Staff
 from .models import Leave
+
 class LeaveForm(forms.ModelForm):
-
-
+    start_date = forms.DateField(required=True)
+    end_date = forms.DateField(required=True)
+    leave_processor = forms.ModelMultipleChoiceField(
+        queryset=Staff.objects.all(), widget = FilteredSelectMultiple('leave processors',False))
     class Meta:
         model = Leave
-        exclude = ['status', ]
-
+        exclude = ['status', 'staff']
+    class Media:
+        css = {
+                'all': (
+                          '/static/admin/css/widgets.css',
+                          '/static/css/widgets.css',
+                       )
+              }
+        js = [
+                '/admin/jsi18n/'
+             ]
     def clean(self):
         cleaned_data = super().clean()
         start_date = cleaned_data.get('from_date')
