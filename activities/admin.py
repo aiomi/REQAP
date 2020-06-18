5b0d74d731b33cc76771626c5ae5e90168285d41
David Onuh <youngstizy@gmail.com>
2020-06-18 15:46:26 +0100

    add str function in TranscriptNote model and register it in admin

--- activities/admin.py
+++ activities/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import  Transcript, TranscriptAttribute
+from .models import Transcript, TranscriptAttribute, TranscriptNote
 # Register your models here.
 admin.site.register(TranscriptAttribute)
 admin.site.register(Transcript)
+admin.site.register(TranscriptNote)
--- activities/models/rnotes.py
+++ activities/models/rnotes.py
@@ -8,3 +8,6 @@ class TranscriptNote(models.Model):
     action = models.CharField(max_length=10)
     reason = models.CharField(max_length=160, blank=True, null=True)
     staff_id = models.ForeignKey(Staff, on_delete=models.DO_NOTHING)
+    
+    def __str__(self):
+        return f'Transcript {self.action} for {self.transcript.request_by}'
